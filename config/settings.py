a5ece83883e86a0531eb0965128fec3b6eaa5c67
Erick-Henrique-Pedrini <erickpedrinebr@gmail.com>
2025-06-18 21:00:19 -0300

    FIX: settings.py

--- config/settings.py
+++ config/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'agronegocio'
+    'rest_framework',
+    'agronegocio',
+    
 ]
 
 MIDDLEWARE = [
